6122804d4985503c357ec8e7e0bf16112417d16c
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2021-09-03 00:41:20 +0100

    package added, sdk started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.Services.Analytics;
+using Unity.Services.Core;
 
 
 public class GameManager : MonoBehaviour
@@ -22,9 +23,10 @@ public class GameManager : MonoBehaviour
 
     bool isPaused = false;
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-      
+        await UnityServices.InitializeAsync();
+
         panelAttract = GameObject.Find("panelAttract");
         panelAttract.SetActive(false);
 
